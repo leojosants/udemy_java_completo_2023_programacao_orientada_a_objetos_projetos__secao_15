2130ff4e39008617f2d2fb811378cbf1c0b0424c
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-02 19:24:58 -0300

    Creating custom exceptions

---
+++ 04CriandoExcecoesPersonalizadas/src/model/entities/Reservation.java
@@ -0,0 +1,82 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	/*
+	 * attributes section
+	 */
+	private Integer room_number;
+	private Date check_in;
+	private Date check_out;
+	private static SimpleDateFormat simple_date_format = new SimpleDateFormat("dd/MM/aaaa");
+
+	/*
+	 * constructors section
+	 */
+	// constructor - overload
+	public Reservation(Integer room_number, Date check_in, Date check_out) {
+		this.setRoomNumber(room_number);
+		this.setCheckIn(check_in);
+		this.setCheckOut(check_out);
+	}
+
+	/*
+	 * getters and setters
+	 */
+	public Integer getRoomNumber() {
+		return this.room_number;
+	}
+
+	public void setRoomNumber(Integer room_number) {
+		this.room_number = room_number;
+	}
+
+	public Date getCheckIn() {
+		return this.check_in;
+	}
+
+	private void setCheckIn(Date check_in) {
+		this.check_in = check_in;
+	}
+
+	public Date getCheckOut() {
+		return this.check_out;
+	}
+
+	private void setCheckOut(Date check_out) {
+		this.check_out = check_out;
+	}
+
+	/*
+	 * methods section
+	 */
+	public long duration() {
+		// retorna a diferenca em milissegundos
+		long diff = this.getCheckOut().getTime() - this.getCheckIn().getTime();
+
+		// converte milissegundos para dias
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDate(Date check_in, Date check_out) {
+		this.setCheckIn(check_in);
+		this.setCheckOut(check_out);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder string_builder = new StringBuilder();
+		string_builder.append(String.format("Quarto %d, ", this.getRoomNumber()));
+		string_builder.append(String.format("entrada ..: %s, ", simple_date_format.format(this.getCheckIn())));
+		string_builder.append(String.format("saída ..: %s, ", simple_date_format.format(this.getCheckOut())));
+		string_builder.append(String.format("%l noites", this.duration()));
+		return string_builder.toString();
+	}
+	
+	public void displayReservation() {
+		System.out.println(toString());
+	}
+}
