75a0c70c0652a74726a02e50fec0c7a12864517a
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-02 18:04:14 -0300

    Try-catch structure

---
+++ 01EstruturaTryCatch/src/application/Program.java
@@ -0,0 +1,81 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = generateInstanceScanner();
+
+		try {
+			System.out.print("\nDigite uma frase ..: ");
+			String phrase = requestPhrase(scanner);
+
+			String[] vector = splitPhrase(phrase);
+
+			System.out.printf("Digite uma posição [0 a %d]..: ", (vector.length - 1));
+			int position = requestPosition(scanner);
+
+			displayData(position, vector);
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			displayInvalidPositionError();
+
+		} catch (InputMismatchException e) {
+			displayInvalidInputError();
+		}
+
+		scanner.close();
+
+		System.out.println("-> fim do programa");
+	}
+
+	/*
+	 * functions section
+	 */
+	private static Scanner generateInstanceScanner() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner;
+	}
+
+	private static String requestPhrase(Scanner scanner) {
+		String phrase = "";
+
+		phrase = scanner.nextLine().trim();
+
+		while (phrase.length() == 0) {
+			System.out.print("-> campo 'Digite uma frase' não pode ser vazio ..: ");
+			phrase = scanner.nextLine().trim();
+		}
+
+		return phrase;
+	}
+
+	private static String[] splitPhrase(String phrase) {
+		String[] split_phrase = phrase.split(" ");
+		return split_phrase;
+	}
+
+	private static int requestPosition(Scanner scanner) {
+		int position = 0;
+
+		position = scanner.nextInt();
+		scanner.nextLine(); // buffer
+
+		return position;
+	}
+
+	private static void displayData(int position, String[] vector) {
+		System.out.printf("Elemento na posição %d ..: %s%n", position, vector[position]);
+	}
+
+	private static void displayInvalidPositionError() {
+		System.out.println("-> erro: posição inválida");
+	}
+
+	private static void displayInvalidInputError() {
+		System.out.println("-> erro: entrada inválida");
+	}
+}
